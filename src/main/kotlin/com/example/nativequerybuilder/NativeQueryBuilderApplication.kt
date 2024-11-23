b40be24ddf8fceb259adb62ecb9d02934c5b482f
jeong-gumin <jgm@paytalab.com>
2024-11-23 19:21:59 +0900

    init project

---
+++ src/main/kotlin/com/example/nativequerybuilder/NativeQueryBuilderApplication.kt
@@ -0,0 +1,11 @@
+package com.example.nativequerybuilder
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class NativeQueryBuilderApplication
+
+fun main(args: Array<String>) {
+    runApplication<NativeQueryBuilderApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/nativequerybuilder/NativeQueryBuilderApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.nativequerybuilder
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NativeQueryBuilderApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
